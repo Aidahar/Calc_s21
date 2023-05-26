426ab317a6043599445e6f467181a66132759193
trudieis <trudieis@student.21-school.ru>
2023-05-26 20:07:21 +0300

    heh

--- src/parse_string.c
+++ src/parse_string.c
@@ -1,7 +1,6 @@
 #include "parse_string.h"
 
 int main(void) {
-  char data[256] = "(2.222**/-*2.22 + 1.2)-1/2*(2^2-2)";
   char data[256] = "(222*2 + 1.2)(12)-1/2*(2^2-2)";
   if (check_brackets(data)) {
     char *notation = calloc(sizeof(char), len_data(data) * 2);
